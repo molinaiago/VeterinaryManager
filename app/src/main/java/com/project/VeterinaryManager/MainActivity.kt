b47c468b5f5b6d8f59a4dae47d266f1442042ef0
Iago Molina Camargo <molinakmargo@hotmail.com>
2023-08-15 17:39:40 -0300

    2HomeScreen
    Creating the home screen and some validations inputs on the login

--- app/src/main/java/com/project/VeterinaryManager/MainActivity.kt
+++ app/src/main/java/com/project/VeterinaryManager/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.project.VeterinaryManager
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.project.VeterinaryManager.databinding.ActivityMainBinding
+import com.project.VeterinaryManager.view.home
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.loginButton.setOnClickListener {
+            val name = binding.editName.text.toString()
+            val password = binding.editPassword.text.toString()
+
+            when {
+                name.isEmpty() -> {
+                    message(it,"Coloque o seu nome!")
+                } password.isEmpty() -> {
+                    message(it,"Preencha a senha!")
+                } password.length<=5 -> {
+                    message(it,"A senha precisa ter no mínimo 6 caracteres!")
+                } else -> {
+
+                }
+            }
+        }
+
     }
+    private fun message(view: View, message: String) {
+        val snackbar = Snackbar.make(view,message,Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF000"))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun navegarToHome(name: String) {
+        val intent = Intent(this, home::class.java)
+        intent.putExtra("nome",name)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/project/VeterinaryManager/view/home.kt
@@ -0,0 +1,12 @@
+package com.project.VeterinaryManager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.VeterinaryManager.R
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
