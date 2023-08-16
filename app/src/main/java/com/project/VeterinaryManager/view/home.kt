007ab10a1522be87b59fce4902c578f1bc7e89cd
Iago Molina Camargo <molinakmargo@hotmail.com>
2023-08-15 21:52:04 -0300

    CreatingHomePage
    First Button to schedule the service

--- app/src/main/java/com/project/VeterinaryManager/view/home.kt
+++ app/src/main/java/com/project/VeterinaryManager/view/home.kt
@@ -3,10 +3,20 @@ package com.project.VeterinaryManager.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.VeterinaryManager.R
+import com.project.VeterinaryManager.databinding.ActivityHomeBinding
 
 class home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.extras?.getString("name")
+
+        binding.userName.text = "Bem-vindo(a),$name"
+
     }
 }
