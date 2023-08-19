26f882222f5985cadda22e0e41bbbd29e7537e61
Iago Molina Camargo <molinakmargo@hotmail.com>
2023-08-19 13:42:38 -0300

    NewButtonMySchedules
    - You can see your schedules now

--- app/src/main/java/com/project/VeterinaryManager/view/home.kt
+++ app/src/main/java/com/project/VeterinaryManager/view/home.kt
@@ -2,11 +2,12 @@ package com.project.VeterinaryManager.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import com.project.VeterinaryManager.R
 import com.project.VeterinaryManager.databinding.ActivityHomeBinding
 
@@ -22,28 +23,55 @@ class home : AppCompatActivity() {
         supportActionBar?.hide()
         val name = intent.extras?.getString("name")
 
+        binding.mySchedulings.setOnClickListener {
+            fetchUserAgendas(name ?: "")
+        }
+
         binding.scheduleButton.setOnClickListener {
             val intent = Intent(this, Scheduling::class.java)
-            intent.putExtra("name",name)
+            intent.putExtra("name", name)
             startActivity(intent)
         }
     }
 
-    fun openDialog(view: View) {
-        val dialogView = layoutInflater.inflate(R.layout.dialog_layout, null)
-        val dialogBuilder = AlertDialog.Builder(this)
-            .setView(dialogView)
-        val alertDialog = dialogBuilder.create()
-        val dialogText = dialogView.findViewById<TextView>(R.id.dialogText)
-        val dialogButton = dialogView.findViewById<Button>(R.id.dialogButton)
+    private fun fetchUserAgendas(name: String) {
+        val db = FirebaseFirestore.getInstance()
 
-        dialogText.text = "INFORMAÇÕES IMPORTANTES:"
+        db.collection("agendamentos")
+            .whereEqualTo("cliente", name)
+            .get()
+            .addOnSuccessListener { documents ->
+                val agendas = mutableListOf<String>()
 
-        dialogButton.setOnClickListener {
-            alertDialog.dismiss()
-        }
-        alertDialog.show()
+                for (document in documents) {
+                    val option = document.getString("option")
+                    val data = document.getString("data")
+                    val hora = document.getString("hora")
+
+                    agendas.add("Animal: $option\n Data: $data\n Hora: $hora\n--------------------------------------")
+                }
+                showMyAgendasPopup(agendas)
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Erro ao buscar as agendas.", Toast.LENGTH_SHORT).show()
+            }
     }
 
 
+    private fun showMyAgendasPopup(agendas: List<String>) {
+        val dialogView = layoutInflater.inflate(R.layout.my_schedules_popup, null)
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+        val alertDialog = dialogBuilder.create()
+
+        val agendasTextView = dialogView.findViewById<TextView>(R.id.agendasTextView)
+        agendasTextView.text = "Suas agendas:\n\n${agendas.joinToString("\n")}"
+
+        val closeButton = dialogView.findViewById<Button>(R.id.closeButton)
+        closeButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
+    }
 }
